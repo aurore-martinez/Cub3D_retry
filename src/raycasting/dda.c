fc3fd56c253cd462394b54c8eaeb3a4ebfc5d944
Aurore Martinez <aumartin@f6r4s2.clusters.42paris.fr>
2025-11-04 15:10:59 +0100

    calcul et formule a comprendre et appliquer

--- src/raycasting/dda.c
+++ src/raycasting/dda.c
@@ -6,9 +6,28 @@
 /*   By: aumartin <aumartin@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/04 10:41:07 by aumartin          #+#    #+#             */
-/*   Updated: 2025/11/04 10:41:24 by aumartin         ###   ########.fr       */
+/*   Updated: 2025/11/04 15:09:59 by aumartin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/cub3d.h"
 
+/* hit = 0;
+while (hit == 0)
+{
+		if (sideDistRow < sideDistCol)
+		{
+			sideDistRow += deltaRow;
+			cellRow += stepRow;
+			side = 0; // on a franchi une frontière “row” (verticale en terme de mur)
+		} else {
+			sideDistCol += deltaCol;
+			cellCol += stepCol;
+			side = 1; // frontière “col”
+		}
+		if (is_wall_cell(game, cellRow, cellCol))
+		{
+			hit = 1;
+		}
+}
+*/
