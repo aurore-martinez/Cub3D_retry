96c8c16511563c61842c4a6097ae1c19ebf895d0
Aurore Martinez <aumartin@f4r2s2.clusters.42paris.fr>
2025-11-20 17:11:02 +0100

    netotyage et reprise des scripts

--- tests/script_Aurore.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes --log-file=valgrind_full.log ./cub3D assets/maps/map_test3.cub
-cat valgrind_full.log
-grep -n \"still reachable\" -n valgrind_full.log
-sed -n '/still reachable/,/====/p' valgrind_full.log
